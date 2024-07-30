2e54a2f6cdb7f3defb368818931004341cfd7604
officialkinglot <146046831+officialkinglot@users.noreply.github.com>
2024-07-29 22:32:52 -0400

    Update LoginSignup.jsx

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
   const login = async () => {
     console.log("Login Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/login", {
+    await fetch("https://trendyworldbackend.onrender.com/login", {
       method: "POST",
       headers: {
         Accept: "application/json", // Corrected to application/json
@@ -37,7 +37,7 @@ const LoginSignup = () => {
   const signup = async () => {
     console.log("Signup Function Executed", formData);
     let responseData;
-    await fetch("http://localhost:4000/signup", {
+    await fetch("https://trendyworldbackend.onrender.com/signup", {
       method: "POST",
       headers: {
         Accept: "application/json", // Corrected to application/json
