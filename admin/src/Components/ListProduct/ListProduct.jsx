727cce744e7a8af0d121f23ea2e8b7456f3a82b8
officialkinglot <146046831+officialkinglot@users.noreply.github.com>
2024-07-29 22:17:22 -0400

    Update ListProduct.jsx

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -6,7 +6,7 @@ const ListProduct = () => {
   const [allProducts, setAllProducts] = useState([]);
 
   const fetchInfo = async () => {
-    await fetch("http://localhost:4000/allproducts")
+    await fetch("https://trendyworldbackend.onrender.com/allproducts")
       .then((res) => res.json())
       .then((data) => { setAllProducts(data); });
   };
@@ -16,7 +16,7 @@ const ListProduct = () => {
   }, []);
 
   const removeProduct = async (id) => {
-    await fetch("http://localhost:4000/removeproduct", {
+    await fetch("https://trendyworldbackend.onrender.com/removeproduct", {
       method: "POST",
       headers: {
         Accept: "application/json",
